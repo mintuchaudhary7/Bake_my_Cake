b66c19797450d0e1f3b18885ca56c5242a886704
mintu kumar <mintuchaudharychd45b@gmail.com>
2024-12-07 11:57:56 +0530

    login page at frontend has brrn added

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function Navbar(){
+    return(
+        <nav>
+            <NavLink to='/'> Home</NavLink>
+            <NavLink to='/about'>About</NavLink>
+            <NavLink to='login'>Login</NavLink>
+        </nav>
+    )
+}
+
+export default Navbar;
